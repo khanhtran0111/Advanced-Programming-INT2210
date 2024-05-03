c5e60f4f2ef58b3671b021132ed5926e3efa06f9
Tran Gia Khanh <90601002+khanhtran0111@users.noreply.github.com>
2024-05-03 15:48:04 +0700

    Add files via upload

---
+++ game/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include "main.h"
+#include "renderer.h"
+
+#include<SDL.h>
+
+int main(int argc, char* args[]) {
+    srand(time(NULL));
+    game.run();
+
+
+    return 0;
+}
---
+++ game/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+
+#include "game.h"
+
+static Game game;
+
+#endif
